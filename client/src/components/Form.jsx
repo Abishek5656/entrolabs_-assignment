94c943598d356f282919cc38c0c6b368ff12094b
Abishek Varma <abishekvarm8929@gmail.com>
2024-06-13 16:40:03 +0530

    updated the form component

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -17,9 +17,10 @@ const Form = () => {
     quantity: "",
     price: "",
   });
+
   const [openModel, setModel] = useState(false);
   const [queryData, setQueryDate] = useState([]);
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
     setFormData({
@@ -29,7 +30,6 @@ const Form = () => {
   };
 
   const getData = async () => {
-    
     try {
       const response = await axios.get(
         `https://api.91.care/pharmap/new/search.php?q=${formData?.name}`
@@ -38,7 +38,6 @@ const Form = () => {
     } catch (error) {
       toast.error(error.message);
     }
-   
   };
 
   useEffect(() => {
@@ -71,14 +70,14 @@ const Form = () => {
   };
 
   const handleSubmit = async (e) => {
-    setLoading(true)
+    setLoading(true);
     e.preventDefault();
     try {
       const response = await axios.post(
         `${import.meta.env.VITE_BACKEND_ENDPOINT}/api/v1/medicine/create`,
         formData
       );
-      console.log(response)
+
       toast.success(response?.data?.message);
     } catch (error) {
       toast.error(error?.response?.data?.message);
@@ -93,11 +92,11 @@ const Form = () => {
       quantity: "",
       price: "",
     });
-    setLoading(false)
+    setLoading(false);
   };
 
   const handleCross = () => {
-    console.log("cross pressed")
+    console.log("cross pressed");
     setFormData({
       name: "",
       manufacturer: "",
@@ -108,49 +107,51 @@ const Form = () => {
       quantity: "",
       price: "",
     });
-  }
+    setModel(false);
+  };
 
   return (
     <div className="max-w-2xl h-full flex flex-col mt-4 mx-3 sm:mx-auto p-2 border border-black-800 border-solid">
       <Title title="New Sku" />
-      <form
-        className="mt-5 h-full w-full space-y-5"
-        onSubmit={handleSubmit}
-      >
+      <form className="mt-5 h-full w-full space-y-5" onSubmit={handleSubmit}>
         {/* Name */}
         <div className="flex flex-col relative">
           <label className="font-bold">Name</label>
           <div className="relative">
-          <input
-            name="name"
-            className=" appearance-none mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-            type="text"
-            placeholder="Enter at least 3 Characters"
-            value={formData.name}
-            onChange={handleChange}
-            onFocus={() => setModel(true)}
-          />
-          <RxCrossCircled className="absolute top-5 right-3" size={15} 
-          onClick={handleCross}
-          />
-          </div>
-        
+            <input
+              name="name"
+              className=" appearance-none mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              type="text"
+              placeholder="Enter at least 3 Characters"
+              value={formData.name}
+              onChange={handleChange}
+              onFocus={() => setModel(true)}
+            />
 
-{openModel ? (
-          queryData?.length > 0 ? (
-            <div className="shadow-2xl absolute top-20 z-30 w-full  h-[250px] overflow-scroll bg-gray-200 space-y-4 p-1 rounded">
-              {queryData?.map((medicine) => {
-                return (
-                  <MenuList
-                    medicine={medicine}
-                    key={uuidv4()}
-                    handleSelectMedicine={handleSelectMedicine}
-                  />
-                );
-              })}
-            </div>
-          ) : null
-        ) : null}
+            {formData.name.length > 0 ? (
+              <RxCrossCircled
+                className="absolute top-5 right-3"
+                size={15}
+                onClick={handleCross}
+              />
+            ) : null}
+          </div>
+
+          {openModel ? (
+            queryData?.length > 0 ? (
+              <div className="shadow-2xl absolute top-20 z-30 w-full  h-[250px] overflow-scroll bg-gray-200 space-y-4 p-1 rounded">
+                {queryData?.map((medicine) => {
+                  return (
+                    <MenuList
+                      medicine={medicine}
+                      key={uuidv4()}
+                      handleSelectMedicine={handleSelectMedicine}
+                    />
+                  );
+                })}
+              </div>
+            ) : null
+          ) : null}
         </div>
 
         {/* Manufacturer */}
@@ -228,7 +229,7 @@ const Form = () => {
             type="number"
             min={0}
             placeholder="quantity"
-            value={Number(formData.quantity)}
+            value={formData.quantity}
             onChange={handleChange}
           />
         </div>
@@ -242,18 +243,18 @@ const Form = () => {
             type="number"
             min={0}
             placeholder="price"
-            value={Number(formData.price)}
+            value={formData.price}
             onChange={handleChange}
           />
         </div>
 
         <div>
           <button
+            disabled={loading}
             type="submit"
-
-            className="group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
+            className={`group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white 
+              ${loading ? "bg-blue-200" : "bg-blue-600 hover:bg-blue-700"}`}
           >
-           
             {loading ? <p>Loading</p> : <p>Submit</p>}
           </button>
         </div>

f8b24d62835d856342438321a00d4742c1872e45
Abishek Varma <abishekvarm8929@gmail.com>
2024-06-13 09:03:02 +0530

    commit

--- server/src/app.js
+++ server/src/app.js
@@ -13,11 +13,15 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 
+app.get("/api/v1/medicine/", () => {
+    return res.status(200).json("Hi from web server")
+})
 
 //routes import
 import medicineRouter from "./routes/medicine.route.js";
 
 // routes declaration
+
 app.use("/api/v1/medicine",  medicineRouter);
 
 export { app }

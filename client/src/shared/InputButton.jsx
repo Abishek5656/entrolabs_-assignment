8c131608d82e7f0fbb8124459dab913b63e1f96b
Abishek Varma <abishekvarm8929@gmail.com>
2024-06-14 20:58:13 +0530

    commit

--- client/src/shared/InputButton.jsx
+++ client/src/shared/InputButton.jsx
@@ -1,23 +1,3 @@
-// import React, { memo } from "react";
-
-// const InputButton = ({ name, label, type, placeholder, value, onChange }) => {
-//   return (
-//     <div className="flex flex-col">
-//       <label className="font-bold">{label}</label>
-//       <input
-//         name={name}
-//         className="appearance-none block mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-//         type={type}
-//         min={0}
-//         placeholder={placeholder}
-//         value={value}
-//         onChange={onChange}
-//       />
-//     </div>
-//   );
-// };
-
-// export default memo(InputButton);
 import React, { memo } from "react";
 import PropTypes from "prop-types";
 
@@ -41,7 +21,7 @@ const InputButton = ({ name, label, type, placeholder, value, onChange }) => {
 InputButton.propTypes = {
   name: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
-  type: PropTypes.oneOf(['text', 'number', 'email', 'password']).isRequired,
+  type: PropTypes.oneOf(['text', 'number', 'email']).isRequired,
   placeholder: PropTypes.string,
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
   onChange: PropTypes.func.isRequired,
--- client/src/shared/Title.jsx
+++ client/src/shared/Title.jsx
@@ -1,13 +1,3 @@
-// import React from 'react'
-
-// const Title = ( { title}) => {
-//   return (
-//     <h1 className="text-2xl uppercase">{title}</h1>
-//   )
-// }
-
-// export default Title
-
 import React from 'react';
 import PropTypes from 'prop-types';
 
